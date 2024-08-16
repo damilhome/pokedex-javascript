2ef89f02b757617d24008b11c28f33d77f51fe65
Danilo <damilhome@gmail.com>
2024-08-16 10:35:50 -0300

    adicionando o html da nova página pelo js ao clicar em algum card do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-const cardButton = document.getElementById('cardButton');
+let pokemonsToUse = [];
 
 const maxRecords = 151;
 const limit = 10;
@@ -8,8 +8,8 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <a id="cardButton" class="cardButton" href="details.html">
-            <li id="${pokemon.name}" class="pokemon ${pokemon.type}">
+        <a id="${pokemon.name}" class="cardButton" href="details.html" onclick="addHtmlDetailPage(this)">
+            <li id="${pokemon.number}" class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 
@@ -24,91 +24,10 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
-function convertPokemonToDetailPage(pokemon) {
-    return `
-    <section class="shortPokemonInfo">
-            <div class="pokemonInfo">
-                <div class="details">
-                    <h1>Bulbasaur</h1>
-                    <ol class="types">
-                        <li class="type">Grass</li>
-                        <li class="type">Poison</li>
-                    </ol>
-                </div>
-                <span class="number">#1</span>
-            </div>
-            <img class="pokemonPhoto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon image">
-        </section>
-        <section class="pokemonDetails">
-            <div class="aboutHolder">
-                <h2>About</h2>
-                <div class="aboutContainer">
-                    <ol>
-                        <li>Name</li>
-                        <li>Height</li>
-                        <li>Weight</li>
-                        <li>Abilities</li>
-                    </ol>
-                    <ol>
-                        <li>Bulbasaur</li>
-                        <li>0.70 cm</li>
-                        <li>6.9kg</li>
-                        <li>Overgrow, Chlorophyl</li>
-                    </ol>
-                </div>
-            </div>
-
-            <div class="statsHolder">
-                <h2>Stats</h2>
-                <div class="statsContainer">
-                    <ol>
-                        <li>HP</li>
-                        <li>Attack</li>
-                        <li>Defense</li>
-                        <li>Sp. Attack</li>
-                        <li>Sp. Defense</li>
-                        <li>Speed</li>
-                        <li>Total</li>
-                    </ol>
-                    <ol>
-                        <li>
-                            <span>45</span>
-                            <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
-                        </li>
-                        <li>
-                            <span>49</span>
-                            <div class="statsBar"><div class="statsPorcentage grass"></div></div>
-                        </li>
-                        <li>
-                            <span>49</span>
-                            <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
-                        </li>
-                        <li>
-                            <span>65</span>
-                            <div class="statsBar"><div class="statsPorcentage grass"></div></div>
-                        </li>
-                        <li>
-                            <span>65</span>
-                            <div class="statsBar"><div class="statsPorcentage grass"></div></div>
-                        </li>
-                        <li>
-                            <span>45</span>
-                            <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
-                        </li>
-                        <li>
-                            <span>318</span>
-                            <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
-                        </li>
-                    </ol>
-                </div>
-            </div>
-        </section>
-        `
-}
-
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons) => {
         pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+        pokemonsToUse = pokemons;
     })
 }
 
@@ -129,6 +48,8 @@ loadMoreButton.addEventListener('click', () => {
 
 })
 
-cardButton.addEventListener('click', () => {
 
-})
+/* 
+document.getElementById('bulbasaur').addEventListener('click', () => {
+    mainContainer.innerHTML = convertPokemonToDetailPage();
+}) */
---
+++ assets/js/mainDetail.js
@@ -0,0 +1,97 @@
+const containerTest = document.getElementById('containerTest');
+
+function convertPokemonToDetailPage() {
+    return `
+            <section class="shortPokemonInfo">
+                <div class="pokemonInfo">
+                    <div class="details">
+                        <h1>Bulbasaur</h1>
+                        <ol class="types">
+                            <li class="type">Grass</li>
+                            <li class="type">Poison</li>
+                        </ol>
+                    </div>
+                    <span class="number">#1</span>
+                </div>
+                <img class="pokemonPhoto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon image">
+            </section>
+            <section class="pokemonDetails">
+                <div class="aboutHolder">
+                    <h2>About</h2>
+                    <div class="aboutContainer">
+                        <ol>
+                            <li>Name</li>
+                            <li>Height</li>
+                            <li>Weight</li>
+                            <li>Abilities</li>
+                        </ol>
+                        <ol>
+                            <li>Bulbasaur</li>
+                            <li>0.70 cm</li>
+                            <li>6.9kg</li>
+                            <li>Overgrow, Chlorophyl</li>
+                        </ol>
+                    </div>
+                </div>
+
+                <div class="statsHolder">
+                    <h2>Stats</h2>
+                    <div class="statsContainer">
+                        <ol>
+                            <li>HP</li>
+                            <li>Attack</li>
+                            <li>Defense</li>
+                            <li>Sp. Attack</li>
+                            <li>Sp. Defense</li>
+                            <li>Speed</li>
+                            <li>Total</li>
+                        </ol>
+                        <ol>
+                            <li>
+                                <span>45</span>
+                                <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
+                            </li>
+                            <li>
+                                <span>49</span>
+                                <div class="statsBar"><div class="statsPorcentage grass"></div></div>
+                            </li>
+                            <li>
+                                <span>49</span>
+                                <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
+                            </li>
+                            <li>
+                                <span>65</span>
+                                <div class="statsBar"><div class="statsPorcentage grass"></div></div>
+                            </li>
+                            <li>
+                                <span>65</span>
+                                <div class="statsBar"><div class="statsPorcentage grass"></div></div>
+                            </li>
+                            <li>
+                                <span>45</span>
+                                <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
+                            </li>
+                            <li>
+                                <span>318</span>
+                                <div class="statsBar"><div class="statsPorcentage psychic"></div></div>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+        `
+}
+
+function addHtmlDetailPage(elemento) {
+    /* const idElemento = elemento.id;
+    let tempPokemon = {};
+    for (let i = 0; i < pokemonsToUse.length; i++) {
+        const element = pokemonsToUse[i].number;
+        if (idElemento === element) {
+            tempPokemon = pokemonsToUse[i];
+        }
+    } */
+    containerTest.innerHTML = convertPokemonToDetailPage();
+}
+
+window.onload = addHtmlDetailPage();
